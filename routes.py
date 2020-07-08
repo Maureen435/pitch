8bc815d74d7006f565e529ad3739f3b4cd172c6d
Maureen435 <mchepkemoi435@gmail.com>
2020-07-08 15:59:16 +0300

    initial commit

--- routes.py
+++ routes.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+posts = [
+    {
+        'author': 'Memzo',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'July 7, 2020'
+    },
+    {
+        'author': 'Kemei',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'July 8, 2020'
+    }
+]
 
 @app.route("/")
-def hello():
-    return "<h1>Home Page</>"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</>"
+    return render_template('about.html', title=About)
 
 if __name__ == '__main__':
     app.run(debug=True)
