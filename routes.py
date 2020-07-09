ad5322c678b4a6616fe9118c130bcddbc79029f3
Maureen435 <mchepkemoi435@gmail.com>
2020-07-09 15:34:46 +0300

    added the models

--- routes.py
+++ routes.py
@@ -8,27 +8,6 @@ app.config['SECRET_KEY'] = '316eae91ff8eb11e29453ae05be9efe6'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-class User(db.model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unque=True, nullable=False)
-    username = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-
 posts = [
     {
         'author': 'Memzo',
