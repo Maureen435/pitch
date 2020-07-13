f827eaabb9f8d74134f1415ca741e32d74d6dd98
Maureen435 <mchepkemoi435@gmail.com>
2020-07-13 17:33:45 +0300

    initial commit

--- forms.py
+++ forms.py
@@ -27,3 +27,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+    class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username=username.data).first()
+
+        if user:
+            raise validationError('The username name is taken.Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('The email is taken.Please choose a different one.')
